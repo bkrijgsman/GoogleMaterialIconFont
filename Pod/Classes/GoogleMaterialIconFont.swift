b62dd836d6f7feb6e6033b9a3a3985dbdffd386c
Salm <quangvu.luu92@gmail.com>
2016-12-11 20:25:19 +0700

    + update syntax to swift 3

--- Pod/Classes/GoogleMaterialIconFont.swift
+++ Pod/Classes/GoogleMaterialIconFont.swift
@@ -16,20 +16,42 @@ public extension String {
 
 public extension NSString {
     public static func materialIcon(font: MaterialIconFont) -> NSString {
-        return NSString(string: String.materialIcon(font))
+        return NSString(string: String.materialIcon(font: font))
+    }
+}
+
+public extension DispatchQueue {
+    
+    private static var _onceTracker = [String]()
+    
+    /**
+     Executes a block of code, associated with a unique token, only once.  The code is thread safe and will
+     only execute the code once even in the presence of multithreaded calls.
+     
+     - parameter token: A unique reverse DNS style name such as com.vectorform.<name> or a GUID
+     - parameter block: Block to execute once
+     */
+    public class func once(token: String, block:(Void)->Void) {
+        objc_sync_enter(self); defer { objc_sync_exit(self) }
+        
+        if _onceTracker.contains(token) {
+            return
+        }
+        
+        _onceTracker.append(token)
+        block()
     }
 }
 
 public extension UIFont {
     public static func materialIconOfSize(size: CGFloat) -> UIFont {
-        var onceToken: dispatch_once_t = 0
         let filename = "MaterialIcons-Regular"
         let fontname = "Material Icons"
 
-        if UIFont.fontNamesForFamilyName(fontname).isEmpty {
-            dispatch_once(&onceToken, { () -> Void in
-                FontLoader.loadFont(filename)
-            })
+        if UIFont.fontNames(forFamilyName: fontname).isEmpty {
+            DispatchQueue.once(token: "GoogleMaterialIconLoadFont") {
+                FontLoader.loadFont(name: filename)
+            }
         }
         
         guard let font = UIFont(name: fontname, size: size) else {
@@ -41,28 +63,28 @@ public extension UIFont {
 
 private class FontLoader {
     class func loadFont(name: String) {
-        let bundle = NSBundle(forClass: FontLoader.self)
+        let bundle = Bundle(for: FontLoader.self)
         let identifier = bundle.bundleIdentifier
         let fileExtension = "ttf"
         
         let url: NSURL?
         if identifier?.hasPrefix("org.cocoapods") == true {
-            url = bundle.URLForResource(name, withExtension: fileExtension, subdirectory: "GoogleMaterialIconFont.bundle")
+            url = bundle.url(forResource: name, withExtension: fileExtension, subdirectory: "GoogleMaterialIconFont.bundle") as NSURL?
         } else {
-            url = bundle.URLForResource(name, withExtension: fileExtension)
+            url = bundle.url(forResource: name, withExtension: fileExtension) as NSURL?
         }
         
         guard let fontURL = url else { fatalError("\(name) not found in bundle") }
         
-        guard let data = NSData(contentsOfURL: fontURL),
-            let provider = CGDataProviderCreateWithCFData(data) else { return }
-        let font = CGFontCreateWithDataProvider(provider)
+        guard let data = NSData(contentsOf: fontURL as URL),
+            let provider = CGDataProvider(data: data) else { return }
+        let font = CGFont(provider)
         
         var error: Unmanaged<CFError>?
         if !CTFontManagerRegisterGraphicsFont(font, &error) {
-            let errorDescription: CFStringRef = CFErrorCopyDescription(error!.takeUnretainedValue())
+            let errorDescription: CFString = CFErrorCopyDescription(error!.takeUnretainedValue())
             let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
-            NSException(name: NSInternalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
+            NSException(name: NSExceptionName.internalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
         }
     }
 }
